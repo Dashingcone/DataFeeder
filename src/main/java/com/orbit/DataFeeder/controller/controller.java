739df389a0edc782262d1dd6bdbf065c8f58d12e
Dashingcone <siddharthvapi456@gmail.com>
2022-09-15 21:19:02 +0530

    Initial commit

---
+++ src/main/java/com/orbit/DataFeeder/controller/controller.java
@@ -0,0 +1,17 @@
+package com.orbit.DataFeeder.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Properties;
+
+@RestController
+@RequestMapping(path = "getData")
+public class controller {
+    @GetMapping
+    public void func()  {
+
+    }
+}
